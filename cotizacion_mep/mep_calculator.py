d45794341911732a94bac9125c53e699c7fe6345
Dante Barba <dantebarba@gmail.com>
2019-12-30 17:00:52 -0300

    version 0.5.0 con volumen funcionando, nuevos parametros, y json corregido. Ademas se fijo Contado Inmediato en la API

--- cotizacion_mep/mep_calculator.py
+++ cotizacion_mep/mep_calculator.py
@@ -1,16 +1,22 @@
 import cotizacion_mep.mongodb_mep_strategy as mongodb_mep_strategy
 
+
 class MEPCalculator():
     def __init__(self, api):
         self._api = api
 
-    def calculate(self, bonds_list):
+    def calculate(self, bonds_list, min_volume=1, min_operations=1, min_volume_usd=1, min_operations_usd=1):
         mep_value_pairs = []
         for bond in bonds_list:
             mep_value_pairs.append(self._api.get_bonds_pair(bond, bond+"D"))
-        return MEPCalculator.lowest_rate(mep_value_pairs)
+        return MEPCalculator.filter_volume(MEPCalculator.lowest_rate(mep_value_pairs), min_volume, min_operations, min_volume_usd, min_operations_usd)
+
+    @staticmethod
+    def filter_volume(mep_value_pairs, min_volume=1, min_operations=1, min_volume_usd=1, min_operations_usd=1):
+        return list(filter(lambda mep_value: (mep_value.bond_ars().volume > min_volume and mep_value.bond_usd().volume > min_volume_usd and mep_value.bond_ars().cantidad_operaciones > min_operations and mep_value.bond_usd().cantidad_operaciones > min_operations_usd), mep_value_pairs))
 
     @staticmethod
     def lowest_rate(mep_value_pairs):
-        mep_value_pairs.sort(cmp=lambda a, b: int(a.mep_value()*1000 - b.mep_value()*1000))
+        mep_value_pairs.sort(cmp=lambda a, b: int(
+            a.mep_value()*1000 - b.mep_value()*1000))
         return mep_value_pairs
--- tests/test_mep_calculator.py
+++ tests/test_mep_calculator.py
@@ -24,6 +24,30 @@ class MepCalculatorTestSuite(unittest.TestCase):
         self.assertEqual(calculated_bonds[0].mep_value(
         ), 40, "Mep value incorrecto: "+str(calculated_bonds[0].mep_value()))
 
+    def test_filter_volume(self):
+        ay24 = bond.Bond("AY24", 1000, 'ARS', 25, 25)
+        ay24d = bond.Bond("AY24D", 25, 'USD', 25, 25)  # 40
+        ao20 = bond.Bond("AO20", 100, 'ARS', 0, 0)
+        ao20d = bond.Bond("AO20D", 30, 'USD', 1, 1)
+        ac17 = bond.Bond("AC17", 2300, 'ARS', 100, 100)
+        ac17d = bond.Bond("AC17D", 32, 'USD', 10000, 10000)  # 71
+        mep1 = mep.MepValue(ay24d, ay24, "last_update")
+        mep2 = mep.MepValue(ao20d, ao20, "Last update")
+        mep3 = mep.MepValue(ac17d, ac17, "Last update")
+        calculated_bonds = calculator.MEPCalculator.lowest_rate(
+            [mep2, mep1, mep3])
+        filtered = calculator.MEPCalculator.filter_volume(
+            calculated_bonds, min_volume=1, min_operations=1, min_volume_usd=1, min_operations_usd=1)
+
+        self.assertEqual(filtered[0].mep_value(
+        ), 40, "Mep value incorrecto: "+str(filtered[0].mep_value()))
+
+        new_filter = calculator.MEPCalculator.filter_volume(
+            filtered, min_volume=25, min_operations=25, min_volume_usd=25, min_operations_usd=25)
+
+        self.assertEqual(new_filter[0].mep_value(
+        ), 71, "Mep value incorrecto: "+str(new_filter[0].mep_value()))
+
 
 if __name__ == '__main__':
     unittest.main()
