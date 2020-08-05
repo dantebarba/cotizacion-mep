a854bdfd43a2b64b945a1c28f500ef23d6a7971a
Dante Barba <dantebarba@gmail.com>
2020-08-05 13:39:46 -0300

    restricting versions to minimum

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='cotizacion_mep',
       author='dantebarba',
       description='cotizaciones del dolar MEP de Argentina',
       long_description=long_description,
-      install_requires=["flask==1.1.1", "requests==2.20.0", "python-dateutil==2.7.5", "apscheduler==3.5.3", "pymongo==3.7.1"],
+      install_requires=["flask==1.1.1", "requests==2.20.0", "python-dateutil==2.7.5", "apscheduler==3.5.3", "pymongo==3.7.1", "itsdangerous==0.24", "Jinja2==2.10.1", "MarkupSafe==0.23"],
       packages=find_packages(exclude=('tests',)),
       entry_points={
           'console_scripts': [
